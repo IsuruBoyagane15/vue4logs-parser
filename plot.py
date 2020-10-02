d6a4d1af60a9b41ca928ce0964d7d55140f44c84
isuruboyagane15 <isuruboyagane15@gmail.com>
2020-10-02 07:20:56 +0530

    Update plotting

--- plot.py
+++ plot.py
@@ -8,23 +8,14 @@ if __name__ == '__main__':
     TYPE = sys.argv[1]
     benchmark_csv = 'results/' + TYPE + '.csv'
 
-    df = pd.read_csv(benchmark_csv, index_col='Dataset').transpose()
+    df = pd.read_csv(benchmark_csv, index_col='Dataset')
     print(df)
 
     for i in df.columns:
-        plt.plot(df.index,df[i], label = i)
+        plt.bar(df.index, df[i], label = i)
     plt.xlabel("Threshold")
     plt.ylabel("PA")
     plt.legend()
     plt.show()
 
-    average = np.average(df,axis=1)
-    y = average
-    plt.plot(df.index,average)
-    plt.xlabel("Threshold")
-    plt.ylabel("Average PA")
-    plt.show()
-
-    # t = 0.75
-    print(pd.DataFrame([df.index,average]))
 
