7abce179b4ea0620d92400060e321cbcf1d12458
isuruboyagane15 <isuruboyagane15@gmail.com>
2020-10-02 07:05:15 +0530

    Update create_benchmark.py to run both configs

--- create_benchmark.py
+++ create_benchmark.py
@@ -15,127 +15,112 @@ BENCHMARK_SETTINGS = {
         'log_file': 'HDFS/HDFS_2k.log',
         'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
         'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
-        'banned_word': []
-
+        'threshold' : 0.27
     },
 
     'Hadoop': {
         'log_file': 'Hadoop/Hadoop_2k.log',
         'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'banned_word': []
-
+        'threshold' : 0.77
     },
 
     'Spark': {
         'log_file': 'Spark/Spark_2k.log',
         'log_format': '<Date> <Time> <Level> <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
-        'banned_word': []
-
+        'threshold' : 0.67
     },
 
     'Zookeeper': {
         'log_file': 'Zookeeper/Zookeeper_2k.log',
         'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
         'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
-        'banned_word': []
-
+        'threshold' : 0.64
     },
 
     'BGL': {
         'log_file': 'BGL/BGL_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
         'regex': [r'core\.\d+'],
-        'banned_word': []
-
+        'threshold' : 0.43
     },
 
     'HPC': {
         'log_file': 'HPC/HPC_2k.log',
         'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
         'regex': [r'=\d+'],
-        'banned_word': []
-
+        'threshold' : 0.34
     },
 
     'Thunderbird': {
         'log_file': 'Thunderbird/Thunderbird_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'banned_word': []
-
+        'threshold' : 0.27
     },
 
     'Windows': {
         'log_file': 'Windows/Windows_2k.log',
         'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
         'regex': [r'0x.*?\s'],
-        'banned_word': []
-
+        'threshold' : 0.67
     },
 
     'Linux': {
         'log_file': 'Linux/Linux_2k.log',
         'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
-        'banned_word': []
-
+        'threshold' : 0.42
     },
 
     'Android': {
         'log_file': 'Android/Android_2k.log',
         'log_format': '<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>',
         'regex': [r'(/[\w-]+)+', r'([\w-]+\.){2,}[\w-]+', r'\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b'],
-        'banned_word': []
-
+        'threshold' : 0.78
     },
 
     'HealthApp': {
         'log_file': 'HealthApp/HealthApp_2k.log',
         'log_format': '<Time>\|<Component>\|<Pid>\|<Content>',
         'regex': [],
-        'banned_word': []
-
+        'threshold' : 0.34
     },
 
     'Apache': {
         'log_file': 'Apache/Apache_2k.log',
         'log_format': '\[<Time>\] \[<Level>\] <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'banned_word': []
-
+        'threshold' : 0.21
     },
 
     'Proxifier': {
         'log_file': 'Proxifier/Proxifier_2k.log',
         'log_format': '\[<Time>\] <Program> - <Content>',
         'regex': [r'<\d+\ssec', r'([\w-]+\.)+[\w-]+(:\d+)?', r'\d{2}:\d{2}(:\d{2})*', r'[KGTM]B'],
-        'banned_word': []
-
+        'threshold' : 0.78
     },
 
     'OpenSSH': {
         'log_file': 'OpenSSH/OpenSSH_2k.log',
         'log_format': '<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'([\w-]+\.){2,}[\w-]+'],
-        'banned_word': []
-
+        'threshold' : 0.59
     },
 
     'OpenStack': {
         'log_file': 'OpenStack/OpenStack_2k.log',
         'log_format': '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>',
         'regex': [r'((\d+\.){3}\d+,?)+', r'/.+?\s', r'\d+'],
-        'banned_word': []
+        'threshold' : 0.67
     },
 
     'Mac': {
         'log_file': 'Mac/Mac_2k.log',
         'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
         'regex': [r'([\w-]+\.){2,}[\w-]+'],
-        'banned_word': []
-
+        'threshold' : 0.68
     },
 }
 
@@ -153,9 +138,8 @@ def search_index(query_log):
     # print("SEARCH QUERY : ", query_log)
     hits = []
     for token in query_log:
-        if token not in BENCHMARK_SETTINGS[DATASET]['banned_word']:
-            if token in INVERTED_INDEX:
-                hits += INVERTED_INDEX[token]
+        if token in INVERTED_INDEX:
+            hits += INVERTED_INDEX[token]
     hit_set = set(hits)
     return list(hit_set)
 
@@ -325,137 +309,140 @@ if __name__ == '__main__':
     BENCHMARK['Dataset'] = list(BENCHMARK_SETTINGS.keys())
     input_dir = 'logs/'
 
-    THRESHOLD = 0.1
+    PAs = []
 
-    while THRESHOLD < 1:
-        PAs_for_threshold = []
+    for DATASET, setting in BENCHMARK_SETTINGS.items():
 
-        print(THRESHOLD)
-        for DATASET, setting in BENCHMARK_SETTINGS.items():
+        indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
+        log_file = os.path.basename(setting['log_file'])
+        headers, regex = generate_logformat_regex(setting['log_format'])
+        df_log = log_to_dataframe(indir + '/' + log_file, regex, headers)
 
-            indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
-            log_file = os.path.basename(setting['log_file'])
-            headers, regex = generate_logformat_regex(setting['log_format'])
-            df_log = log_to_dataframe(indir + '/' + log_file, regex, headers)
+        if TYPE == '0':
+            THRESHOLD = BENCHMARK_SETTINGS[DATASET]['threshold']
+        elif TYPE == '1':
+            THRESHOLD = 0.6
+        else:
+            print("Error in configs")
+            sys.exit(0)
 
-            for idx, line in df_log.iterrows():
-                logID = line['LineId']
-                pre_processed_log = preprocess(DATASET, line['Content']).strip().split()
-                # print(logID, pre_processed_log)
-                pre_processed_log = replace_alpha_nums(pre_processed_log)
+        for idx, line in df_log.iterrows():
+            logID = line['LineId']
+            pre_processed_log = preprocess(DATASET, line['Content']).strip().split()
+            # print(logID, pre_processed_log)
+            pre_processed_log = replace_alpha_nums(pre_processed_log)
 
-                log_line = replace_alpha_nums(pre_processed_log)
-                log_line = filter_wildcards(log_line)
-                # print("FILTERED LOG LINE :", log_line)
+            log_line = replace_alpha_nums(pre_processed_log)
+            log_line = filter_wildcards(log_line)
+            # print("FILTERED LOG LINE :", log_line)
 
-                hits = search_index(log_line)
-                found = False
-                if len(hits) > 0:
-                    for hit in hits:
-                        if pre_processed_log == TEMPLATES[hit]:
-                            # print("early catch")
-                            RESULTS.append(hit)
-                            found = True
+            hits = search_index(log_line)
+            found = False
+            if len(hits) > 0:
+                for hit in hits:
+                    if pre_processed_log == TEMPLATES[hit]:
+                        # print("early catch")
+                        RESULTS.append(hit)
+                        found = True
 
-                if found:
-                    continue
-                # print("more rules")
+            if found:
+                continue
+            # print("more rules")
 
-                # IF NO CANDIDATE FOUND
-                if len(hits) == 0:
-                    # print("NO HITS")
+            # IF NO CANDIDATE FOUND
+            if len(hits) == 0:
+                # print("NO HITS")
+                new_id = get_new_template(pre_processed_log)
+                index_doc(new_id)
+
+            # IF THERE IS AT LEAST ONE CANDIDATE
+            else:
+                candidates = {key: TEMPLATES[key] for key in hits}
+                length_filtered_candidates = {key: candidates[key] for key in candidates if
+                                              len(candidates[key]) == len(pre_processed_log)}
+                if len(length_filtered_candidates) == 0:
                     new_id = get_new_template(pre_processed_log)
                     index_doc(new_id)
-
-                # IF THERE IS AT LEAST ONE CANDIDATE
                 else:
-                    candidates = {key: TEMPLATES[key] for key in hits}
-                    length_filtered_candidates = {key: candidates[key] for key in candidates if
-                                                  len(candidates[key]) == len(pre_processed_log)}
-                    if len(length_filtered_candidates) == 0:
-                        new_id = get_new_template(pre_processed_log)
-                        index_doc(new_id)
-                    else:
-                        candidate_found = False
-                        for i in length_filtered_candidates:
-                            if pre_processed_log == TEMPLATES[i]:
-                                RESULTS.append(i)
-                                candidate_found = True
-                                break
+                    candidate_found = False
+                    for i in length_filtered_candidates:
+                        if pre_processed_log == TEMPLATES[i]:
+                            RESULTS.append(i)
+                            candidate_found = True
+                            break
 
-                        if not candidate_found:
-                            max_similarity = 0
-                            selected_candidate_id = None
-                            remaining_hits = list(length_filtered_candidates.keys())
+                    if not candidate_found:
+                        max_similarity = 0
+                        selected_candidate_id = None
+                        remaining_hits = list(length_filtered_candidates.keys())
 
-                            TEMPLATES[-1] = pre_processed_log
-                            doc_ids = [-1]
-                            for hit in length_filtered_candidates:
-                                doc_ids.append(hit)
+                        TEMPLATES[-1] = pre_processed_log
+                        doc_ids = [-1]
+                        for hit in length_filtered_candidates:
+                            doc_ids.append(hit)
 
-                            cosine_similarity = get_tfidf(doc_ids, TEMPLATES)[0]
+                        cosine_similarity = get_tfidf(doc_ids, TEMPLATES)[0]
 
-                            TEMPLATES[-1] = None
-                            for i in range(len(cosine_similarity)):
-                                if i == 0:
-                                    continue
-                                else:
-                                    current_similarity = cosine_similarity[i]
-                                    if current_similarity > max_similarity:
-                                        max_similarity = current_similarity
-                                        selected_candidate_id = remaining_hits[i - 1]
-
-                            if max_similarity < THRESHOLD:
-                                new_id = get_new_template(pre_processed_log)
-                                index_doc(new_id)
+                        TEMPLATES[-1] = None
+                        for i in range(len(cosine_similarity)):
+                            if i == 0:
+                                continue
                             else:
-                                selected_candidate = TEMPLATES[selected_candidate_id]
+                                current_similarity = cosine_similarity[i]
+                                if current_similarity > max_similarity:
+                                    max_similarity = current_similarity
+                                    selected_candidate_id = remaining_hits[i - 1]
 
-                                if pre_processed_log == selected_candidate:
-                                    # print("SELECTED TEMPLATE IS EQUAL TO LOG LINE")
-                                    RESULTS.append(selected_candidate_id)
-                                else:
-                                    template_length = len(selected_candidate)
-                                    # print("SELECTED TEMPLATE IS not EQUAL TO LOG LINE")
-                                    temporary_tokens = []
-                                    changed_tokens = []
+                        if max_similarity < THRESHOLD:
+                            new_id = get_new_template(pre_processed_log)
+                            index_doc(new_id)
+                        else:
+                            selected_candidate = TEMPLATES[selected_candidate_id]
 
-                                    for index in range(template_length):
-                                        # if log_line_token_list[position] == candidate_token_list[position]:
-                                        if pre_processed_log[index] == selected_candidate[index] or \
-                                                "<*>" in selected_candidate[index]:
-                                            temporary_tokens.append(selected_candidate[index])
-                                        else:
-                                            changed_tokens.append(selected_candidate[index])
-                                            temporary_tokens.append("<*>")
-                                    updated_template = temporary_tokens
-                                    update_doc(changed_tokens, selected_candidate_id)
+                            if pre_processed_log == selected_candidate:
+                                # print("SELECTED TEMPLATE IS EQUAL TO LOG LINE")
+                                RESULTS.append(selected_candidate_id)
+                            else:
+                                template_length = len(selected_candidate)
+                                # print("SELECTED TEMPLATE IS not EQUAL TO LOG LINE")
+                                temporary_tokens = []
+                                changed_tokens = []
 
-                                    TEMPLATES[selected_candidate_id] = updated_template
-                                    RESULTS.append(selected_candidate_id)
+                                for index in range(template_length):
+                                    # if log_line_token_list[position] == candidate_token_list[position]:
+                                    if pre_processed_log[index] == selected_candidate[index] or \
+                                            "<*>" in selected_candidate[index]:
+                                        temporary_tokens.append(selected_candidate[index])
+                                    else:
+                                        changed_tokens.append(selected_candidate[index])
+                                        temporary_tokens.append("<*>")
+                                updated_template = temporary_tokens
+                                update_doc(changed_tokens, selected_candidate_id)
 
-                    assert len(RESULTS) == logID
+                                TEMPLATES[selected_candidate_id] = updated_template
+                                RESULTS.append(selected_candidate_id)
 
-                # print("==== TEMPLATES ====")
-                # for t in TEMPLATES:
-                #     print(TEMPLATES[t])
-                # print("\n")
+                assert len(RESULTS) == logID
 
-            write_results()
+            # print("==== TEMPLATES ====")
+            # for t in TEMPLATES:
+            #     print(TEMPLATES[t])
+            # print("\n")
 
-            ground_truth_df = 'ground_truth/' + DATASET + '_2k.log_structured.csv'
-            output = "results/" + BENCHMARK_NAME + "/" + str(
-                THRESHOLD) + "/" + DATASET + "_structured.csv"  # results_ is for manual inverted index results
-            print(DATASET + ' :', end='')
-            pa = evaluate(ground_truth_df, output)[1]
-            PAs_for_threshold.append(pa)
+        write_results()
 
-            RESULTS = []
-            INVERTED_INDEX = {}
-            TEMPLATES = {}
+        ground_truth_df = 'ground_truth/' + DATASET + '_2k.log_structured.csv'
+        output = "results/" + BENCHMARK_NAME + "/" + str(
+            THRESHOLD) + "/" + DATASET + "_structured.csv"  # results_ is for manual inverted index results
+        print(DATASET + ' :', end='')
+        pa = evaluate(ground_truth_df, output)[1]
+        PAs.append(pa)
 
-        BENCHMARK[THRESHOLD] = PAs_for_threshold
-        THRESHOLD = round(THRESHOLD + 0.1, 2)
+        RESULTS = []
+        INVERTED_INDEX = {}
+        TEMPLATES = {}
 
+    BENCHMARK['PA'] = PAs
     print(BENCHMARK)
+    print("Average :", round(sum(PAs)/16.0, 4))
     BENCHMARK.to_csv('results/' + BENCHMARK_NAME + '.csv', index=False)
