409d4f211135272e6fdcc1a2e6821c106f1062de
isuruboyagane15 <isuruboyagane15@gmail.com>
2020-10-20 07:55:31 +0530

    Add both test run configs in CreateBenchmark

--- CreateBenchmark.py
+++ CreateBenchmark.py
@@ -1,6 +1,17 @@
 from Vue4logsParser import *
 
 if __name__ == '__main__':
+    type = sys.argv[1]
+    pas = []
     for dataset, setting in benchmark_settings.items():
-        parser = Vue4Logs(0.78, dataset)
-        parser.parse()
+        if type == '1':
+            parser = Vue4Logs(0.78, dataset)
+        elif type == '0':
+            parser = Vue4Logs(benchmark_settings[dataset]['threshold'], dataset)
+        else:
+            print("Error in arguments.")
+
+        pa = parser.parse()
+        pas.append(pa)
+
+    print(sum(pas)/16.0)
