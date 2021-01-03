1c6c6b3f951df08e9ce8025d02b8ed79e72073c8
isuruboyagane15 <isuruboyagane15@gmail.com>
2021-01-03 16:47:31 +0530

    Update benchmark output

--- CreateBenchmark.py
+++ CreateBenchmark.py
@@ -1,5 +1,8 @@
 from Vue4logsParser import *
 
+import random
+random.seed(3)
+
 if __name__ == '__main__':
     type = sys.argv[1]
     try:
@@ -7,22 +10,42 @@ if __name__ == '__main__':
     except IndexError:
         dataset_chosen = None
     pas = []
+
     for dataset, setting in benchmark_settings.items():
+        ground_truth_df = 'ground_truth/' + dataset + '_2k.log_structured.csv'
+
         if dataset_chosen is not None and dataset != dataset_chosen:
             continue
+
         if type == '1':
-            threshold = 0.78
+            threshold = 0.6
+            parser = Vue4Logs(threshold, dataset)
+            parser.parse()
+            output = "results/" + str(threshold) + "/" + dataset + "_structured.csv"
+            pa = evaluate(ground_truth_df, output)[1]
+            pas.append(pa)
+
         elif type == '0':
-            threshold = benchmark_settings[dataset]['threshold']
+            highest_pa_for_dataset = 0
+            selected_threshold = None
+
+            for i in range(10):
+                random_threshold = round(random.random(), 2)
+                parser = Vue4Logs(random_threshold, dataset)
+                parser.parse()
+                output = "results/" + str(random_threshold) + "/" + dataset + "_structured.csv"
+                pa = evaluate(ground_truth_df, output)[1]
+                if pa > highest_pa_for_dataset:
+                    highest_pa_for_dataset = pa
+                    selected_threshold = random_threshold
+
+            selected_output = "results/" + str(selected_threshold) + "/" + dataset + "_structured.csv"
+            selected_pa = evaluate(ground_truth_df, selected_output)[1]
+            print(dataset, selected_pa)
+            pas.append(selected_pa)
+
         else:
             print("Error in arguments.")
             sys.exit(0)
-
-        parser = Vue4Logs(threshold, dataset)
-        parser.parse()
-        ground_truth_df = 'ground_truth/' + dataset + '_2k.log_structured.csv'
-        output = "results/" + str(threshold) + "/" + dataset + "_structured.csv"
-        pa = evaluate(ground_truth_df, output)[1]
-        pas.append(pa)
 
     print(sum(pas)/16.0)
--- Vue4logsParser.py
+++ Vue4logsParser.py
@@ -15,112 +15,96 @@ benchmark_settings = {
         'log_file': 'HDFS/HDFS_2k.log',
         'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
         'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
-        'threshold': 0.27
     },
 
     'Hadoop': {
         'log_file': 'Hadoop/Hadoop_2k.log',
         'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'threshold': 0.77
     },
 
     'Spark': {
         'log_file': 'Spark/Spark_2k.log',
         'log_format': '<Date> <Time> <Level> <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
-        'threshold': 0.67
     },
 
     'Zookeeper': {
         'log_file': 'Zookeeper/Zookeeper_2k.log',
         'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
         'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
-        'threshold': 0.64
     },
 
     'BGL': {
         'log_file': 'BGL/BGL_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
         'regex': [r'core\.\d+'],
-        'threshold': 0.43
     },
 
     'HPC': {
         'log_file': 'HPC/HPC_2k.log',
         'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
         'regex': [r'=\d+'],
-        'threshold': 0.34
     },
 
     'Thunderbird': {
         'log_file': 'Thunderbird/Thunderbird_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'threshold': 0.27
     },
 
     'Windows': {
         'log_file': 'Windows/Windows_2k.log',
         'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
         'regex': [r'0x.*?\s'],
-        'threshold': 0.67
     },
 
     'Linux': {
         'log_file': 'Linux/Linux_2k.log',
         'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
-        'threshold': 0.42
     },
 
     'Android': {
         'log_file': 'Android/Android_2k.log',
         'log_format': '<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>',
         'regex': [r'(/[\w-]+)+', r'([\w-]+\.){2,}[\w-]+', r'\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b'],
-        'threshold': 0.78
     },
 
     'HealthApp': {
         'log_file': 'HealthApp/HealthApp_2k.log',
         'log_format': '<Time>\|<Component>\|<Pid>\|<Content>',
         'regex': [],
-        'threshold': 0.34
     },
 
     'Apache': {
         'log_file': 'Apache/Apache_2k.log',
         'log_format': '\[<Time>\] \[<Level>\] <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'threshold': 0.23
     },
 
     'Proxifier': {
         'log_file': 'Proxifier/Proxifier_2k.log',
         'log_format': '\[<Time>\] <Program> - <Content>',
         'regex': [r'<\d+\ssec', r'([\w-]+\.)+[\w-]+(:\d+)?', r'\d{2}:\d{2}(:\d{2})*', r'[KGTM]B'],
-        'threshold': 0.78
     },
 
     'OpenSSH': {
         'log_file': 'OpenSSH/OpenSSH_2k.log',
         'log_format': '<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'([\w-]+\.){2,}[\w-]+'],
-        'threshold': 0.59
     },
 
     'OpenStack': {
         'log_file': 'OpenStack/OpenStack_2k.log',
         'log_format': '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>',
         'regex': [r'((\d+\.){3}\d+,?)+', r'/.+?\s', r'\d+'],
-        'threshold': 0.67
     },
 
     'Mac': {
         'log_file': 'Mac/Mac_2k.log',
         'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
         'regex': [r'([\w-]+\.){2,}[\w-]+'],
-        'threshold': 0.68
     },
 }
 input_dir = 'logs/'
@@ -431,7 +415,6 @@ class Vue4Logs:
                         self.templates[selected_candidate_id] = updated_template
                         self.results.append(selected_candidate_id)
                 assert len(self.results) == log_id
-        print("\n", self.dataset, len(list(self.templates.keys())))
         self.write_results()
 
 
